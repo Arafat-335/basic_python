3c2d919753628c15a6791df4cb157596f1c17e6c
Arafat_web <arafathossein335@gmail.com>
2025-09-28 21:10:16 +0600

    calculator

---
+++ calculator.py
@@ -0,0 +1,171 @@
+#problam 1 
+
+#user input number for sum/Addition 
+
+num1 = int(input("Enter first number: ")) 
+
+num2 = int(input("Enter second number: ")) 
+
+print(f"{num1} + {num2} = {num1+num2}") 
+
+ 
+ 
+
+#problam 2 
+
+#user input number for minus/Subtraction 
+
+num1 = int(input("Enter first number: ")) 
+
+num2 = int(input("Enter second number: ")) 
+
+print(f"{num1}-{num2}={num1-num2}") 
+
+ 
+ 
+
+#problam 3 
+
+#user input number for Multiplication/times 
+
+num1 = int(input("Enter first number: ")) 
+
+num2 = int(input("Enter second number: ")) 
+
+print(f"{num1} * {num2} = {num1 * num2}") 
+
+ 
+ 
+
+#problam 4 
+
+#user input number for Division 
+
+num1 = int(input("Enter first number: ")) 
+
+num2 = int(input("Enter second number: ")) 
+
+print(f"{num1} / {num2} = {num1 / num2}") 
+
+ 
+ 
+
+#problam 5 
+
+#user input number for sum/Addition with fungtion 
+
+def calculator(a,b): 
+
+    sum = a + b 
+
+    print(sum) 
+
+     
+
+calculator(3,5) 
+
+calculator(20,5) 
+
+calculator(3,95) 
+
+calculator(3,545) 
+
+calculator(33,5) 
+
+calculator(3,54) 
+
+calculator(39,5) 
+
+ 
+ 
+ 
+
+#problam 6 
+
+#user input number for minus/Subtraction with function 
+
+def calculator(a,b): 
+
+    minus = a - b 
+
+    print(minus) 
+
+     
+
+calculator(3,5) 
+
+calculator(20,5) 
+
+calculator(3,95) 
+
+calculator(3,545) 
+
+calculator(33,5) 
+
+calculator(3,54) 
+
+calculator(39,5) 
+
+ 
+ 
+ 
+
+#problam 7 
+
+#user input number for Multiplication/times with function 
+
+def calculator(a,b): 
+
+    times = a * b 
+
+    print(times) 
+
+     
+
+calculator(3,5) 
+
+calculator(20,5) 
+
+calculator(3,95) 
+
+calculator(3,545) 
+
+calculator(33,5) 
+
+calculator(3,54) 
+
+calculator(39,5) 
+
+ 
+ 
+ 
+
+#problam 8 
+
+#user input number for Division with function 
+
+def calculator(a,b): 
+
+    division = a / b 
+
+    print(division) 
+
+     
+
+calculator(3,5) 
+
+calculator(20,5) 
+
+calculator(3,95) 
+
+calculator(3,545) 
+
+calculator(33,5) 
+
+calculator(3,54) 
+
+calculator(39,5) 
+
+ 
+
+ 
