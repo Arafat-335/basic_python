86bfa323a66a4e4a211fd67748ffffcd3abc4cd3
Arafat_web <arafathossein335@gmail.com>
2025-09-30 02:34:45 +0600

    Update Quiz_game.py

--- Quiz_game.py
+++ Quiz_game.py
@@ -1,50 +1,35 @@
 # problam 1 
 
-questions = ("What is Python language?",) 
 
-options = ( 
 
-    "a. Python is a programming language", 
+print("wellcome to my python game !!!") 
 
-    "b. This is a human language", 
+quiz = [
+	{"question": "your name?","ans": "b"},
+	{"question": "where are your city","ans":"c"},
+	{"question": "Your favorite color?","ans":"a"},
+	{"question":"Your hobby?","ans":"b"},
+]
+options = [["a) Rafi", "b) Arafat", "c) Hamin", "d) Emon"],
+           ["a) Dhaka", "b) Sylhet", "c) Chittagong", "d) Khulna"],
+           ["a) Blue", "b) Red", "c) Green", "d) Black"],
+           ["a) Reading", "b) Playing games", "c) Traveling", "d) Watching movies"],
+]
 
-    "c. This is only one computer language" 
+score = 0
 
-) 
+def check_answer(user_guess, correct_ans):
+	return user_guess == correct_ans
 
-answers = ("a",) 
-
-guesses = [] 
-
-score = 0 
-
-questions_num = 0 
-
-for question in questions: 
-
-    print(question) 
-
-    for option in options: 
-
-        print(option) 
-
-    guess = input("Enter (a/b/c): ") 
-
-    guesses.append(guess) 
-
-    if guess == answers[questions_num]: 
-
-        score += 1 
-
-        print("Correct!") 
-
-    else: 
-
-        print("Incorrect!") 
-
-    questions_num += 1 
-
-print(f"\nYour score is: {score}/{len(questions)}") 
-
- 
- 
+for question_num in range(len(quiz)):
+	print("")
+	print(quiz[question_num]["question"])
+	for i in options[question_num]:
+		print(i)
+	guess = input("enter a/b/c/d: ").lower()
+	is_correct = check_answer(guess, quiz[question_num]["ans"])
+	if is_correct:
+		print("correct answer")
+		score += 1
+	else:
+		print("incorrect answer")
