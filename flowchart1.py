bee53770fa5dee1e6dd6de09c4fa430926be298e
Arafat_web <arafathossein335@gmail.com>
2025-10-12 12:41:11 +0600

    user input

--- flowchart1.py
+++ flowchart1.py
@@ -1,169 +1,41 @@
-# 3 input flowchart
-
-# a = int(input("enter your number: "))
-# b = int(input("enter your number: "))
-# c = int(input("enter your number: "))
-
-# fast_number = None
-# second_number = None
-# third_number = None
-
-# if a>b and a>c:
-#     fast_number = a
-    
-#     if b>c:
-#         second_number = b
-#         third_number = c
-#     else:
-#         second_number = c
-#         third_number = b
-        
-# else:
-#     if b>c and b>a:
-#         fast_number = b
-        
-#         if a>c:
-#             second_number = a
-#             third_number = c
-#         else:
-#             second_number = c
-#             third_number = a
-#     else:
-#         fast_number = c
-#         if a>b:
-#             second_number = a
-#             third_number = b
-#         else:
-#             second_number = b
-#             third_number = a
-            
-# print(fast_number)
-# print(second_number)
-# print(third_number)
-
-
-# 4 input flowchart
-a = int(input("Enter your number: "))
-b = int(input("Enter your number: "))
-c = int(input("Enter your number: "))
-d = int(input("Enter your number: "))
+a = int(input("enter your number"))
+b = int(input("enter your number"))
+c = int(input("enter your number"))
+d = int(input("enter your number"))
+e = int(input("enter your number"))
 
 first_number = None
 second_number = None
 third_number = None
 fourth_number = None
+Fifth_number = None
 
-
-if a > b and a > c and a > d:
+if a>b and a>c and a>d and a>e:
     first_number = a
-    if b > c and b > d:
+    if b>c and b>d and b>e:
         second_number = b
-        if c > d:
+        if c>d and c>e:
             third_number = c
-            fourth_number = d
-        else:
-            third_number = d
-            fourth_number = c
-    elif c > b and c > d:
-        second_number = c
-        if b > d:
-            third_number = b
-            fourth_number = d
-        else:
-            third_number = d
-            fourth_number = b
-    else:
-        second_number = d
-        if b > c:
-            third_number = b
-            fourth_number = c
-        else:
-            third_number = c
-            fourth_number = b
+            if d>e:
+                fourth_number = d
+                Fifth_number = e
+            else:
+                fourth_number = e
+                Fifth_number = d
 
-elif b > a and b > c and b > d:
+elif b>c and b>d and b>e and b>a:
     first_number = b
-    if a > c and a > d:
-        second_number = a
-        if c > d:
-            third_number = c
-            fourth_number = d
-        else:
-            third_number = d
-            fourth_number = c
-    elif c > a and c > d:
+    if c>d and c>e and c>a:
         second_number = c
-        if a > d:
-            third_number = a
-            fourth_number = d
-        else:
+        if d>e and d>a:
             third_number = d
-            fourth_number = a
-    else:
-        second_number = d
-        if a > c:
-            third_number = a
-            fourth_number = c
-        else:
-            third_number = c
-            fourth_number = a
-
-elif c > a and c > b and c > d:
-    first_number = c
-    if a > b and a > d:
-        second_number = a
-        if b > d:
-            third_number = b
-            fourth_number = d
-        else:
-            third_number = d
-            fourth_number = b
-    elif b > a and b > d:
-        second_number = b
-        if a > d:
-            third_number = a
-            fourth_number = d
-        else:
-            third_number = d
-            fourth_number = a
-    else:
-        second_number = d
-        if a > b:
-            third_number = a
-            fourth_number = b
-        else:
-            third_number = b
-            fourth_number = a
-
-else:
-    first_number = d
-    if a > b and a > c:
-        second_number = a
-        if b > c:
-            third_number = b
-            fourth_number = c
-        else:
-            third_number = c
-            fourth_number = b
-    elif b > a and b > c:
-        second_number = b
-        if a > c:
-            third_number = a
-            fourth_number = c
-        else:
-            third_number = c
-            fourth_number = a
-    else:
-        second_number = c
-        if a > b:
-            third_number = a
-            fourth_number = b
-        else:
-            third_number = b
-            fourth_number = a
+            if e>a:
+                fourth_number = e
+                Fifth_number = a
+            else:
+                fourth_number = a
+                Fifth_number = e
 
 
 print(first_number)
 print(second_number)
-print(third_number)
-print(fourth_number)
---
+++ input.py
@@ -0,0 +1,33 @@
+# input () method 
+# a = input()
+# print(a)
+
+
+# input () method with a massage
+# a = input("Enter your number: ")
+# print(a)
+
+# a = input()
+# print("what is your name ?" " " + a)
+# print(a)
+
+# a = input()
+# print("my name is ")
+# print(a)
+
+
+# typecasting the user input
+# a = int(input("Enter your number: "))
+# print(type(a))
+# print(a+2)
+
+# a = int(input("enter your number: "))
+# b = int(input("enter your number: "))
+# c = (a + b)
+# print(c)
+
+# a = input()
+# print(int(a)+int(a))
+
+a = input()
+print(f"my name is {a}")
